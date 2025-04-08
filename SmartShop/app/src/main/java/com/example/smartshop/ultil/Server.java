79b211eca38a1003d35cee67cce847eca12114fe
s0nbuildtech287 <buixu4ns0n@gmail.com>
2025-04-08 23:51:28 +0700

    update

--- SmartShop/app/src/main/java/com/example/smartshop/ultil/Server.java
+++ SmartShop/app/src/main/java/com/example/smartshop/ultil/Server.java
@@ -1,7 +1,7 @@
 package com.example.smartshop.ultil;
 
 public class Server {
-    public static String localhost = "192.168.0.103";
+    public static String localhost = "192.168.90.102";
     public static String DuongDanLoaiSP = "http://"  + localhost + "/ServerSmartShop/Main/getLoaiSP.php";
     public static String DuongDanLogin = "http://"  + localhost + "/ServerSmartShop/LoginRegister/login.php";
     public static String DuongDanSignUp = "http://"  + localhost + "/ServerSmartShop/LoginRegister/signup.php";
