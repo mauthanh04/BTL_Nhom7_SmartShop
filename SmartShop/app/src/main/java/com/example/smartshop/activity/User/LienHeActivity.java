adc57dcc2eb0d26abe5a70ed5d4a0ca750af5776
Nguyễn Mậu Thành <mauthanh00004@gmail.com>
2025-04-10 23:52:08 +0700

    update

--- SmartShop/app/src/main/java/com/example/smartshop/activity/User/LienHeActivity.java
+++ SmartShop/app/src/main/java/com/example/smartshop/activity/User/LienHeActivity.java
@@ -1,21 +1,50 @@
 package com.example.smartshop.activity.User;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import com.example.smartshop.R;
 
 public class LienHeActivity extends AppCompatActivity {
 
+    Toolbar toolbarlienhe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lien_he);
+        toolbarlienhe = findViewById(R.id.toolbarlienhe);
+        ActionToolbar();
+    }
+
+    private void ActionToolbar(){
+        if (toolbarlienhe != null) {
+            setSupportActionBar(toolbarlienhe);
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                Drawable navIcon = toolbarlienhe.getNavigationIcon();
+                if (navIcon != null) {
+                    navIcon = DrawableCompat.wrap(navIcon);
+                    DrawableCompat.setTint(navIcon, Color.WHITE);
+                    toolbarlienhe.setNavigationIcon(navIcon);
+                }
+            }
+            toolbarlienhe.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        } else {
+            Log.e("ToolbarError", "Toolbar is null");
+        }
     }
 }
--- SmartShop/app/src/main/java/com/example/smartshop/activity/User/ThongTinActivity.java
+++ SmartShop/app/src/main/java/com/example/smartshop/activity/User/ThongTinActivity.java
@@ -1,25 +1,33 @@
 package com.example.smartshop.activity.User;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.smartshop.R;
 import com.example.smartshop.databinding.ActivityThongTinBinding;
 
-public class ThongTinActivity extends FragmentActivity {
+public class ThongTinActivity extends AppCompatActivity {
 
     private ActivityThongTinBinding binding;
+    Toolbar toolbarthongtin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityThongTinBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        toolbarthongtin = findViewById(R.id.toolbarthongtin);
+        ActionToolbar();
         // Xử lý nút "Mở bản đồ"
         binding.openMapButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,4 +52,28 @@ public class ThongTinActivity extends FragmentActivity {
             }
         });
     }
+
+    private void ActionToolbar(){
+        if (toolbarthongtin != null) {
+            setSupportActionBar(toolbarthongtin);
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                Drawable navIcon = toolbarthongtin.getNavigationIcon();
+                if (navIcon != null) {
+                    navIcon = DrawableCompat.wrap(navIcon);
+                    DrawableCompat.setTint(navIcon, Color.WHITE);
+                    toolbarthongtin.setNavigationIcon(navIcon);
+                }
+            }
+            toolbarthongtin.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        } else {
+            Log.e("ToolbarError", "Toolbar is null");
+        }
+    }
+
 }
